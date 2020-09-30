b7323dc69b513350c8076df0850acaf464b56e66
Hrithik Jha <hrithik.jha2017@vitstudent.ac.in>
2020-09-30 20:19:36 +0530

    Add frontend

---
+++ static/main.js
@@ -0,0 +1,32 @@
+const input = document.querySelector('#textarea')
+const messages = document.querySelector('#messages')
+const username = document.querySelector('#username')
+const send = document.querySelector('#send')
+
+const url = "ws://" + window.location.host + "/ws";
+const ws = new WebSocket(url);
+
+ws.onmessage = (msg) => {
+    insertMessage(JSON.parse(msg.data));
+};
+
+send.onclick = () => {
+    const message = {
+        username: username.value,
+        message: input.value,
+    }
+
+    ws.send(JSON.stringify(message));
+    input.value = "";
+};
+
+insertMessage = (messageObj) => {
+    const message = document.createElement('div');
+
+    message.setAttribute('class', 'chat-message')
+    console.log("name: " + messageObj.username + " content: " + messageObj.message)
+    message.textContent = `${messageObj.username}: ${messageObj.content}`
+
+    message.appendChild(message)
+    messages.insertBefore(message, messages.firstChild)
+}
